5c61331ff0824cc7b9670ce201db95b82fbe1efb
sjxiang <1535484943@qq.com>
2022-11-16 22:37:38 +0800

    opcode finish

---
+++ README.md
@@ -0,0 +1,3 @@
+## barinfsck，同 brainfuck
+
+cargo run -- ./bf_code/hello_world.bf
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use opcode::{Opcode, Code};
+
+mod opcode;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = std::env::args().collect();
+    println!("{}", &args[1]);
+
+    let data = std::fs::read(&args[1])?;
+
+    // let data_str = std::str::from_utf8(&data).unwrap();
+    let code = Code::from(data)?;
+    println!("{:?}", code.instrs);
+
+    Ok(())
 }
